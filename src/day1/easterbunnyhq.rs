21c8cd97a57fdfd26081ef0602b67435a85bf66e
ForsakenHarmony <ForsakenHarmony@users.noreply.github.com>
2016-12-03 00:12:07 +0100

    idk, nothing works

--- src/day1/easterbunnyhq.rs
+++ src/day1/easterbunnyhq.rs
@@ -6,23 +6,40 @@ enum Direction {
 }
 
 struct Runner {
-  facing: Direction,
-  position: Vec<i32>
+  pub facing: Direction,
+  pub position: Vec<i32>,
+  pub steps: Vec<&'static str>
+}
+
+impl Runner {
+  pub fn new(steps: Vec<&str>) -> Runner {
+    Runner{
+      facing: Direction::North,
+      position: vec!(0,0),
+      steps: steps
+    }
+  }
 }
 
 pub fn find_pos(input: &str) -> Vec<i32> {
-  let split: Vec<&str> = input.split(", ").collect();
+  let split: Vec<&'static str> = input.split(", ").collect();
+  
+  
+  
+  let mut runner = Runner::new(split);
+  
+  turn(runner, true);
   
   println!("{:?}", split);
   
   vec!(0, 0)
 }
 
-pub fn turn(r: &Runner, right: bool) {
+fn turn(r: Runner, right: bool) {
   let new_facing = match r.facing {
-    Direction::North => if right {Direction::East} else {Direction::West}
-    Direction::East => if right {Direction::South} else {Direction::North}
-    Direction::South => if right {Direction::West} else {Direction::East}
-    Direction::West => if right {Direction::North} else {Direction::South}
+    Direction::North => if right { Direction::East } else { Direction::West },
+    Direction::East => if right { Direction::South } else { Direction::North },
+    Direction::South => if right { Direction::West } else { Direction::East },
+    Direction::West => if right { Direction::North } else { Direction::South }
   };
 }
